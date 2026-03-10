a02eb1aadc592966973eb161f04f557a39d957dd
techtreespace <84021299+techtreespace@users.noreply.github.com>
2026-03-10 11:32:32 +0900

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: '/thanks1004/',  // ← 여기에 본인 GitHub 저장소 이름을 정확히 입력!
   plugins: [react()],
   test: {
     environment: "jsdom",
