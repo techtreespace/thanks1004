50d23c10aa1dc58f72d6682e398c36e3287e3511
techtreespace <84021299+techtreespace@users.noreply.github.com>
2026-03-10 11:47:25 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";  // ← 추가
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>  {/* ← App을 HashRouter로 감싸기 */}
+    <App />
+  </HashRouter>
+);
